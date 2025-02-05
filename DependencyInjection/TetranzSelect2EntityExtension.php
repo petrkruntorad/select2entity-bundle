cf31915a486dc0460e375377ddcbd5a7dab1ebf0
Petr Kruntorád <petrlunarion@gmail.com>
2025-02-05 12:44:52 +0100

    Resolved The "Symfony\Component\HttpKernel\DependencyInjection\Extension" class is considered internal since Symfony 7.1, to be deprecated in 8.1; use Symfony\Component\DependencyInjection\Extension\Extension instead. It may change without further notice.

--- DependencyInjection/TetranzSelect2EntityExtension.php
+++ DependencyInjection/TetranzSelect2EntityExtension.php
@@ -5,7 +5,7 @@ namespace Tetranz\Select2EntityBundle\DependencyInjection;
 use Exception;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
